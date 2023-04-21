303366ad5beb02c77d70424cff6bed56535ac48b
SparkyPX <pierrelucproulx@gmail.com>
2023-04-21 12:55:36 -0400

    Javascript code cleanup

--- script/script.js
+++ script/script.js
@@ -1,31 +1,32 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
-window.addEventListener('scroll', (event) => {
-   body.classList.add('is-scrolling');
-   setTimeout(function(){
-      body.classList.remove('is-scrolling');
-   }, 100);
-   console.log("La page scroll!");
-
+window.addEventListener("scroll", (event) => {
+  body.classList.add("is-scrolling");
+  setTimeout(function () {
+    body.classList.remove("is-scrolling");
+  }, 100);
+  console.log("La page scroll!");
 });
 gsap.registerPlugin(ScrollTrigger);
 const Scroll = document.querySelector(".mouse");
 
 const fullScreenElement = document.querySelector("#fullscreen-div");
-document.querySelector('.toggle-fullscreen-btn').addEventListener("click", () => {
-if (document.fullscreenElement) {
-    document.exitFullscreen();
- } else {
-    fullScreenElement.requestFullscreen();
- }
-});
+document
+  .querySelector(".toggle-fullscreen-btn")
+  .addEventListener("click", () => {
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      fullScreenElement.requestFullscreen();
+    }
+  });
 /* Animation de l'icône scroll de l'intro*/
-gsap.to('.mouse',{
-duration: '1',
-delay: '0.5',
-y: '110px',
-rotation: '180',
-stagger: '0.1',
-yoyo: true,
-repeat: '-1'
+gsap.to(".mouse", {
+  duration: "1",
+  delay: "0.5",
+  y: "110px",
+  rotation: "180",
+  stagger: "0.1",
+  yoyo: true,
+  repeat: "-1",
 });
